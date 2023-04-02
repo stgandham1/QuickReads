fa6a45b67263b91c860e782f1958c11db6bbc2bb
Nat W <nkcwai@gmail.com>
2023-04-02 16:53:14 -0400

    Add Frontend Files
    These are frontend files created with Expo SDK. I so that I could open it on web.

---
+++ frontend/LoginPage.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Text} from 'react-native';
+
+export default function LoginPage() { 
+    // Call Server with route
+    return (
+        <html> 
+            <h1>LOG IN Here</h1>
+        </html>
+    )
+}
