3311feb9ed64ca16edc46e5e96bb4a59461fbec7
TheSearchGeeks <krishmalhotra@thesearchgeeks.com>
2023-04-02 16:27:53 -0400

    first

--- server.js
+++ server.js
@@ -6,13 +6,36 @@ app.get('/', async (req,res) => {
     res.send("Welcome to the Quickreads")
   });
   
-  app.get('/read', async (req,res) => {
+app.get('/read', async (req,res) => {
     let x = 5
     let y = 5
     let z = x + y
     res.send("" + z)
   });
+
+let arr = [{name: 'krish', username: "krish67"}, {name: "tommy", username:"tommy123"}]
+
+app.get('/getName/:name', async (req,res) =>
+{
+ let n = req.params.name
+ let u = null
+ for (user in arr){
+  if (user.name === n){
+    u = user
+  }
+ }
+
+res.send(u)
+}
+
+
+);
+
   
-  
+
+
+
+
+
   app.listen(8080, () => {console.log("Running")});
   
