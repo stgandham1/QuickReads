558d509a17fada41e77418df9d6356122c70a323
y-bhargava <yashbhargava1219@gmail.com>
2023-04-05 16:41:59 -0400

    Updating server.js

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require ('express');
 const app = express();
+var Pool = require('pg-pool')
+
+var pool = new Pool({
+  database: 'database-1',
+  user: 'brianc',
+  password: 'secret!',
+  port: 5432,
+  ssl: true,
+  max: 20, // set pool max size to 20
+  idleTimeoutMillis: 1000, // close idle clients after 1 second
+  connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+  maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
+})
   
 
 app.get('/', async (req,res) => {
@@ -10,6 +23,12 @@ app.get('/read', async (req,res) => {
     let x = 5
     let y = 5
     let z = x + y
+    res.send("" + z)
+  });
+
+app.get('/getcategories/:username',async (req,res) => {
+    let user = req.params.username
+    
     res.send("" + z)
   });
 
