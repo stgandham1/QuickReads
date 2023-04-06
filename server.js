a311028e69d233638a19bb85f04bc0fd0332b6fe
TheSearchGeeks <krishmalhotra@thesearchgeeks.com>
2023-04-06 17:32:22 -0400

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const app = express();
-var Pool = require('pg-pool')
+const Pool = require('pg').Pool
 
 var pool = new Pool({
   database: 'postgres',
@@ -9,21 +9,20 @@ var pool = new Pool({
   host:'quickreads.caksjqa6dpcu.us-east-2.rds.amazonaws.com',
   port: 5432,
 })
-  
+pool.connect();
 
 app.get('/', async (req,res) => {
     res.send("Welcome to the Quickreads")
   });
   
 app.get('/verifyAuthentication/:username/:password', async  (req,res) => {
-  const client = await pool.connect();
   const username = req.params.username
   const password = req.params.password
   const query = {
           text: 'SELECT * FROM authentication WHERE username = $1 AND password = $2',
           values: [username, password],
         };
-  const result = await client.query(query);
+  const result = await pool.query(query);
   res.send(result.rows.length > 0)
 });
 
