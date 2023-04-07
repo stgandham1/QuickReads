c86a2989ef8e47f53ad1bbb7d3daee6a4d90532e
Nat W <nkcwai@gmail.com>
2023-04-07 18:35:42 -0400

    Cleaning Up Pages

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import LoginPage from "./screens/LoginPage";
-import HomePage from "./pages/HomePage";
-import BottomTabNavigator from "./routes/bottomTab";
-import Home from "./screens/home";
-import ReviewDetail from "./screens/reviewDetails";
 import { LoginStackNavigator } from "./routes/loginStack";
 
-const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import LoginPage from "./screens/LoginPage";
+// import BottomTabNavigator from "./routes/bottomTab";
+// import Home from "./screens/home";
+// import ReviewDetail from "./screens/reviewDetails";
 
 export default function App() {
-  // const [loaded] = useFonts({
-  //   "n-regular": require("./assets/fonts/Nunito-Regular.ttf"),
-  //   "n-bold": require("./assets/fonts/Nunito-Bold.ttf"),
-  // });
-  // if (!loaded) {
-  //   return null; //If Fonts not Loaded, return Null
-  // }
   const [loaded] = useFonts({
     "n-regular": require("./assets/fonts/Nunito-Regular.ttf"),
     "n-bold": require("./assets/fonts/Nunito-Bold.ttf"),
@@ -32,22 +22,9 @@ export default function App() {
   }
   return (
     <NavigationContainer>
-      {/* <BottomTabNavigator /> */}
       <LoginStackNavigator/>
     </NavigationContainer>
   );
-  // return (
-  //   <NavigationContainer>
-  //     <Stack.Navigator>
-  //       {/* NATHANIEL'S COMPONENTS: LOGIN AND HOME */}
-  //       <Stack.Screen options = {{headerShown: false}} name="Login" component={LoginPage} />
-  //       <Stack.Screen options = {{headerShown: false}} name="Home" component={HomePage} />
-  //       {/* HAORAN'S COMPONENTS: ARTICLE CONTAINER (HOME) AND ARTICLE REVIEW DETAILS*/}
-  //       {/* <Stack.Screen  name="HomePage" component={Home} />
-  //       <Stack.Screen  name="ReviewDetail" component={ReviewDetail} /> */}
-  //     </Stack.Navigator>
-  //   </NavigationContainer>
-  // );
 }
 
 const styles = StyleSheet.create({
