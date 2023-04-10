87b71a3001aedeac55b00b5f296ce940b59dc8f7
TheSearchGeeks <krishmalhotra@thesearchgeeks.com>
2023-04-10 14:42:17 -0400

    test

--- server.js
+++ server.js
@@ -45,11 +45,10 @@ app.get('/', async (req,res) => {
 
   // Add Category
   app.get('/addcategory/:username/:category', async (req,res) => {
-    currentcategories = await pool.query("SELECT category from public.categories WHERE username=$1",[req.params.username])
+    currentcategories = await pool.query("SELECT category from public.categories WHERE username=$1",[JSON.parsereq.params.username])
     currentcategories = currentcategories.rows[0].category
     currentcategories.push(req.params.category)
-    
-    let test = await pool.query("UPDATE public.categories SET category = $1 WHERE username = $2", [JSON.stringify({category: currentcategories}), req.params.username]);
+    let test = await pool.query("UPDATE public.categories SET category = $1 WHERE username = $2", [JSON.stringify(currentcategories), req.params.username]);
     res.send(test);
   });
 
