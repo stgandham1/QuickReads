0f7a28784a2cfa8dae637bce392d6f4e7db71c64
nowfuture011130 <105948127+nowfuture011130@users.noreply.github.com>
2023-04-12 11:42:24 -0400

    delete withoutfeedback

--- frontend/screens/Login.js
+++ frontend/screens/Login.js
@@ -6,7 +6,6 @@ import {
   View,
   Keyboard,
   TouchableOpacity,
-  TouchableWithoutFeedback,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -15,7 +14,7 @@ export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigation = useNavigation();
-  const [errorText, setErrorText] = useState(""); 
+  const [errorText, setErrorText] = useState("");
 
   useEffect(() => {
     /** if (logged in)
@@ -29,103 +28,117 @@ export default function LoginPage() {
     navigation.replace("BottomTabNavigator");
   };
 
-  const signupRoute = 'http://quickreads-env.eba-nmhvwvfp.us-east-1.elasticbeanstalk.com/adduser';
-  const loginRoute = 'http://quickreads-env.eba-nmhvwvfp.us-east-1.elasticbeanstalk.com/checkuser';
-  
+  const signupRoute =
+    "http://quickreads-env.eba-nmhvwvfp.us-east-1.elasticbeanstalk.com/adduser";
+  const loginRoute =
+    "http://quickreads-env.eba-nmhvwvfp.us-east-1.elasticbeanstalk.com/checkuser";
+
   async function handleSignUp() {
-    if (email.length == 0 || password.length == 0) { 
+    if (email.length == 0 || password.length == 0) {
       console.log("[No Input Detected]");
       return;
     }
 
     let usernameExists = false;
-    const loginRequest = await fetch(loginRoute+'/'+email+'/'+password, {
-      method:'GET',
-    }).then(response => {return response.json()})
-    .then((responseJSON) => {
-        if (responseJSON.status  || (responseJSON.message != 'Username does not exist')) {
+    const loginRequest = await fetch(
+      loginRoute + "/" + email + "/" + password,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => {
+        if (
+          responseJSON.status ||
+          responseJSON.message != "Username does not exist"
+        ) {
           usernameExists = true;
         }
-    }).catch(); 
-    if (usernameExists) { 
+      })
+      .catch();
+    if (usernameExists) {
       setErrorText("Username Already Exists");
-      return; 
+      return;
     }
     setErrorText("Registered " + email + "!");
     //ADD USER TO TABLE
-    const signupRequest = await fetch(signupRoute+'/'+email+'/'+password, {
-      method:'GET', 
-    })
-  };
+    const signupRequest = await fetch(
+      signupRoute + "/" + email + "/" + password,
+      {
+        method: "GET",
+      }
+    );
+  }
 
   async function handleLogin() {
-    if (email.length == 0 || password.length == 0) { 
+    if (email.length == 0 || password.length == 0) {
       console.log("[No Input Detected]");
       return;
     }
-    const loginRequest = await fetch(loginRoute+'/'+email+'/'+password, {
-      method:'GET',
-    }).then((response) => {return response.json();})
-    .then((responseJSON) => {
+    const loginRequest = await fetch(
+      loginRoute + "/" + email + "/" + password,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => {
         if (responseJSON.status) {
           setErrorText("Logging in " + email);
           goHome();
         } else {
           setErrorText("Could not log in " + email);
         }
-    })
-  };
+      });
+  }
 
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        console.log("dismiss keyborad");
-        Keyboard.dismiss();
-      }}
-    >
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Email"
-            value={email}
-            onChangeText={(input) => setEmail(input)}
-            style={styles.input}
-          />
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(input) => setEmail(input)}
+          style={styles.input}
+        />
 
-          <TextInput
-            placeholder="Password"
-            value={password}
-            onChangeText={(input) => setPassword(input)}
-            style={styles.input}
-            secureTextEntry
-          />
-        </View>
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(input) => setPassword(input)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
 
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            onPress={handleLogin}
-            style={[styles.button, styles.buttonOutline]}
-          >
-            <Text style={styles.button}>Login</Text>
-          </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.button}>Login</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity
-            onPress={handleSignUp}
-            style={[styles.button, styles.buttonEmpty]}
-          >
-            <Text style={styles.button}>Signup</Text>
-          </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonEmpty]}
+        >
+          <Text style={styles.button}>Signup</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity
-            onPress={goHome}
-            style={[styles.button, styles.buttonEmpty]}
-          >
-            <Text style={styles.button}>No Account</Text>
-          </TouchableOpacity>
-          <Text style={styles.error}>{errorText}</Text>
-        </View>
-      </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
+        <TouchableOpacity
+          onPress={goHome}
+          style={[styles.button, styles.buttonEmpty]}
+        >
+          <Text style={styles.button}>No Account</Text>
+        </TouchableOpacity>
+        <Text style={styles.error}>{errorText}</Text>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -168,13 +181,12 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingHorizontal: 10,
     paddingVertical: 5,
-    borderColor: '#bbd8fc',
+    borderColor: "#bbd8fc",
     borderWidth: 2,
-    marginTop:10,
-  }, 
-  error: { 
-    color:'grey',
-    marginTop:10,
-
-  }
+    marginTop: 10,
+  },
+  error: {
+    color: "grey",
+    marginTop: 10,
+  },
 });
