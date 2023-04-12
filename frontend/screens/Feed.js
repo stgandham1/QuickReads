37c70a91d246bdce803f8d0725daf06ae810c7f2
nowfuture011130 <105948127+nowfuture011130@users.noreply.github.com>
2023-04-12 08:54:35 -0400

    submit handler

--- frontend/screens/Feed.js
+++ frontend/screens/Feed.js
@@ -5,6 +5,7 @@ import {
   Text,
   FlatList,
   TouchableOpacity,
+  Button,
 } from "react-native";
 import { globalStyles } from "../styles/global";
 import { articles } from "../articles";
@@ -13,26 +14,55 @@ export default function Feed({ navigation }) {
   const [reviews, setReviews] = useState(articles);
   //GET ARTICLES FROM BACKEND
 
-  async function refreshArticles() { 
-    let username = 'nat'; //PLACEHOLDER UNTIL USERNAME PROP CAN BE PASSED IN
-    let feedRoute = 'http://quickreads-env.eba-nmhvwvfp.us-east-1.elasticbeanstalk.com/getarticles'
-    const articleRequest = await fetch(feedRoute+'/'+username, {
-      method:'GET',
-    }).then(response => {return response.json()})
-    .then((responseJSON) => {
+  const submitHandler = (text) => {
+    setReviews((preText) => {
+      return [text, ...preText];
+    });
+  };
+  //Passing an article constent to the submitHandler
+  //adds it to the article holder
+  //below is an example
+  // const pressHandler2 = () => {
+  //   submitHandler({
+  //     title: "article 1",
+  //     content:
+  //       "article 1 content\naaaa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa\naa",
+  //     tags: ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6"],
+  //     key: "1",
+  //   });
+  // };
+
+  const deleteHandler = () => {
+    setReviews((preText) => {
+      return [];
+    });
+  };
+  //deletinng all the articles
+
+  async function refreshArticles() {
+    let username = "nat"; //PLACEHOLDER UNTIL USERNAME PROP CAN BE PASSED IN
+    let feedRoute =
+      "http://quickreads-env.eba-nmhvwvfp.us-east-1.elasticbeanstalk.com/getarticles";
+    const articleRequest = await fetch(feedRoute + "/" + username, {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => {
         console.log(responseJSON);
-        for(var key in responseJSON) {
-          articles.unshift({title: responseJSON[key]['title'],
-          content: responseJSON[key]['summary'],
-          tags: ["tag1", "tag2", "tag3"],
-          key: key,})
+        for (var key in responseJSON) {
+          articles.unshift({
+            title: responseJSON[key]["title"],
+            content: responseJSON[key]["summary"],
+            tags: ["tag1", "tag2", "tag3"],
+            key: key,
+          });
         }
-
-    }).catch();
-
+      })
+      .catch();
   }
   refreshArticles();
-
 
   const pressHandler = () => {
     navigation.navigate("ReviewDetail");
@@ -49,7 +79,7 @@ export default function Feed({ navigation }) {
             <Text style={globalStyles.homeText}>{item.title}</Text>
           </TouchableOpacity>
         )}
-        ItemSeparatorComponent={() => <View style={{height: 30}} />}
+        ItemSeparatorComponent={() => <View style={{ height: 30 }} />}
       />
     </View>
   );
