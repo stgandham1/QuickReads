07e33d9ae9012336ed9e3da1bb579b93cf7ad13d
Saket Gandham <118041366+stgandham1@users.noreply.github.com>
2023-04-20 14:12:09 -0400

    new feed using paper

--- frontend/screens/Newsletter.js
+++ frontend/screens/Newsletter.js
@@ -1,112 +1,21 @@
-import React, { useState, useEffect } from "react";
+import * as React from 'react';
+import { Avatar, Button, Card, Text } from 'react-native-paper';
 
-import { StyleSheet, View, Text, LayoutAnimation, UIManager,  Platform, TouchableOpacity} from "react-native";
-import { globalStyles } from "../styles/global";
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
-// import Icon from 'react-native-vector-icons/Ionicons';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+const MyComponent = () => (
+  <Card>
+    <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+    <Card.Content>
+      <Text variant="titleLarge">Card title</Text>
+      <Text variant="bodyMedium">Card content</Text>
+    </Card.Content>
+    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    <Card.Actions>
+      <Button>Cancel</Button>
+      <Button>Ok</Button>
+    </Card.Actions>
+  </Card>
+);
 
-if (
-  Platform.OS === "android" &&
-  UIManager.setLayoutAnimationEnabledExperimental
-) {
-  UIManager.setLayoutAnimationEnabledExperimental(true);
-}
-
-const Accordion = ({ title, children }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleOpen = () => {
-    setIsOpen(value => !value);
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-  }
-
-  return (
-    <>
-      <TouchableOpacity onPress={toggleOpen} style={styles.heading} activeOpacity={0.6}>
-        {title}
-      </TouchableOpacity>
-      <View style={[styles.list, !isOpen ? styles.hidden : undefined]}>
-        {children}
-      </View>
-    </>
-  );
-};
-
-
-export default function Newsletter() {
-  const title=(
-    <View>
-    <Text style={styles.sectionTitle} >Profile</Text>
-    <Text style={styles.sectionDescription} >Address, Contact</Text>
-    </View>
-  )
-  const body = (
-    <View>
-      <Text style={styles.sectionTitle} >Profile</Text>
-      <Text style={styles.sectionDescription} >Address, Contact</Text>
-      <Text style={styles.sectionTitle} >Profile</Text>
-      <Text style={styles.sectionDescription} >Address, Contact</Text>
-    </View>
-  )
-  return (
-
-      <View style={styles.container}>
-        <Text style={styles.text}>Animated Accordion/ Drawer/ Drop-down/ Collapsible-card</Text>
-        <Accordion title={title} >
-          {body}
-        </Accordion>
-        <View style={{alignItems: 'center'}} ><View style={styles.divider} /></View>
-        <Accordion title={title} >
-          {body}
-        </Accordion>
-        <View style={{alignItems: 'center'}} ><View style={styles.divider} /></View>
-        <Accordion title={title} >
-          {body}
-        </Accordion>
-        <View style={{alignItems: 'center'}} ><View style={styles.divider} /></View>
-        
-      </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 15,
-    paddingVertical: 30,
-  },
-  text: {
-    fontSize: 18,
-    marginBottom: 20,
-  },
-  safeArea: {
-    flex: 1,
-  },
-  heading: {
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 10
-  },
-  hidden: {
-    height: 0,
-  },
-  list: {
-    overflow: 'hidden'
-  },
-  sectionTitle: {
-    fontSize: 16,
-    height: 30,
-    marginLeft: '5%',
-  },
-  sectionDescription: {
-    fontSize: 12,
-    height: 30,
-    marginLeft: '5%',
-  },
-  divider: {
-    borderBottomColor: 'grey',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    width: '100%',
-},
-});
+export default MyComponent;
