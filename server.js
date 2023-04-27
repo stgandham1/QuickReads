180c9a60466f785d94b2058202f7c0558d9eb893
TheSearchGeeks <krishmalhotra@thesearchgeeks.com>
2023-04-27 16:41:30 -0400

    try

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ const pool = new Pool({
 
 pool.connect()
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get('/', async (req,res) => {
     res.send("Welcome to the Quickreads")
   });
