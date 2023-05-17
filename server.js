1739bacbeb9c64be70225582450741b8aa96e2c8
y-bhargava <86212377+y-bhargava@users.noreply.github.com>
2023-05-17 08:10:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -195,6 +195,7 @@ app.get('/', async (req,res) => {
       for(let key in result) {
         result[key] = [...new Set(result[key])];
       }
+      res.send(result)
       console.log(keys)
       for(let lang in result) {
         // Get the value corresponding to the key and loop through it
